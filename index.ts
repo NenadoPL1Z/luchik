c6ae9cf5fe6d59ab1bbf13ad67d4441466466323
NenadoPL1Z <limon4ikpro1@gmail.com>
2025-03-19 05:38:20 +0500

    feat: update app

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { AppRegistry, LogBox } from "react-native";
 import { decode, encode } from "base-64";
-import App from "./App";
+import { App } from "@app/app/app";
 import { name as appName } from "./app.json";
 
 if (!global.btoa) global.btoa = encode;
---
+++ src/app/app.tsx
@@ -0,0 +1,10 @@
+import { Animated, SafeAreaView } from "react-native";
+import Text = Animated.Text;
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>123</Text>
+    </SafeAreaView>
+  );
+};
