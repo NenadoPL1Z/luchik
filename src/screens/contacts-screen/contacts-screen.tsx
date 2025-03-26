f1276d3eb1612792f59a353361df67123bf40799
NenadoPL1Z <limon4ikpro1@gmail.com>
2025-03-26 20:44:13 +0500

    feat: update link

--- src/screens/contacts-screen/contacts-screen.tsx
+++ src/screens/contacts-screen/contacts-screen.tsx
@@ -17,17 +17,17 @@ export const ContactsScreen = ({ navigation }: ContactsScreenProps) => {
             649100, Республика Алтай,{"\n"}Майминский район, село Майма,{"\n"}улица Ленина, 8А
           </TextBlock>
           <TextBlock icon="CallIcon24" title="Телефон">
-            <Typography variant="p" onPress={() => Linking.openURL("tel:83882220506")}>
-              +7(38822)2-05-06
-            </Typography>
+            <TouchableOpacity onPress={() => Linking.openURL("tel:83882220506")}>
+              <Typography variant="p">+7(38822)2-05-06</Typography>
+            </TouchableOpacity>
             {"\n"}
-            <Typography variant="p" onPress={() => Linking.openURL("tel:89039192809")}>
-              +79039192809
-            </Typography>
+            <TouchableOpacity onPress={() => Linking.openURL("tel:89039192809")}>
+              <Typography variant="p">+79039192809</Typography>
+            </TouchableOpacity>
             {"\n"}
-            <Typography variant="p" onPress={() => Linking.openURL("tel:83884423402")}>
-              +7(38844)23-4-02
-            </Typography>
+            <TouchableOpacity onPress={() => Linking.openURL("tel:83884423402")}>
+              <Typography variant="p">+7(38844)23-4-02</Typography>
+            </TouchableOpacity>
           </TextBlock>
           <TextBlock icon="WorldIcon24" title="Вебсайт">
             <TouchableOpacity onPress={() => Linking.openURL("https://www.socza.ru")}>
