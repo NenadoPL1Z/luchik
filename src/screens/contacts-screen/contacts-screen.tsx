3d61f210cd97596e28c9d29a08074c087aaf982d
NenadoPL1Z <limon4ikpro1@gmail.com>
2025-03-28 00:05:19 +0500

    fix: phone

--- src/screens/contacts-screen/contacts-screen.tsx
+++ src/screens/contacts-screen/contacts-screen.tsx
@@ -16,15 +16,15 @@ export const ContactsScreen = ({ navigation }: ContactsScreenProps) => {
             649100, Республика Алтай,{"\n"}Майминский район, село Майма,{"\n"}улица Ленина, 8А
           </TextBlock>
           <TextBlock icon="CallIcon24" title="Телефон">
-            <TouchableOpacity onPress={() => Linking.openURL("tel:83882220506")}>
+            <TouchableOpacity style={styles.phone} onPress={() => Linking.openURL("tel:83882220506")}>
               <Typography variant="p">+7(38822)2-05-06</Typography>
             </TouchableOpacity>
             {"\n"}
-            <TouchableOpacity onPress={() => Linking.openURL("tel:89039192809")}>
+            <TouchableOpacity style={styles.phone} onPress={() => Linking.openURL("tel:89039192809")}>
               <Typography variant="p">+79039192809</Typography>
             </TouchableOpacity>
             {"\n"}
-            <TouchableOpacity onPress={() => Linking.openURL("tel:83884423402")}>
+            <TouchableOpacity style={styles.phone} onPress={() => Linking.openURL("tel:83884423402")}>
               <Typography variant="p">+7(38844)23-4-02</Typography>
             </TouchableOpacity>
           </TextBlock>
